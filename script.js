1bbff0cc9ce003d828bb4081e1f8b02cd2cefe72
Jeison <102593994+Jtomio@users.noreply.github.com>
2022-05-06 15:10:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const chk = decument.getElementById('chk')
 
-chk.addEventListener('change', ()=> {
-    document.body.classList.toggle('dark')
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
 })
+
+// calculadora 50/30/20
+
+function clicar() {
+  var num1 = document.querySelector(".num1").value;
+  
+  var resultado1 = parseInt(num1) / 2 ;
+   document.querySelector(".resultado50").innerHTML = resultado1;
+  var resultado2 = parseInt(num1) * 0.3
+   document.querySelector(".resultado30").innerHTML = resultado2;
+  var resultado3 = parseInt(num1) * 0.2
+   document.querySelector(".resultado20").innerHTML = resultado3;
+      
+}
+
+
